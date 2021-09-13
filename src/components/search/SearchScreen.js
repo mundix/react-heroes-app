1b0f6698b4aaea9949993998ec299fe8b3aee447
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-13 05:01:18 -0400

    Fixed getHeroByName validartion when name is empty and add exceptions labels when query is empty or hearoesFiltered came empty

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import queryString from 'query-string';
-import { heroes } from '../../data/heroes';
 import { HeroCard } from '../heroes/HeroCard';
 import { useForm } from '../../hooks/useForm';
 import { useLocation } from 'react-router';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
-export const SearchScreen = ({history}) => {
+export const SearchScreen = ({ history }) => {
 
     const location = useLocation();
-
-    const { q } = queryString.parse(location.search);
+    // Si es undefined, voy a poner un string vacio para que no de error. 
+    const { q = '' } = queryString.parse(location.search);
 
     // Para poder trabajar con el query String, se recomienda un paquete
     // https://www.npmjs.com/package/query-string
-
 
     const [formValues, handleInputChange] = useForm({
         // query: '',
@@ -21,9 +20,12 @@ export const SearchScreen = ({history}) => {
         query: q,
     });
 
-    const {query} = formValues;
+    const { query } = formValues;
 
-    const heroesFiltered = heroes;
+    //Memorizamos la busqueda, solo cambia cuando el query de la url 'q' cambie,
+    // si lo pongo con el input query , se generan consoles o eventos. 
+    const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+    // const heroesFiltered = getHeroesByName(query);
 
 
     const handleSearch = (e) => {
@@ -31,29 +33,30 @@ export const SearchScreen = ({history}) => {
         history.push(`?q=${query}`);
         console.log(formValues);
     };
-    
+
     return (
         <div>
             <h1>Search Screen</h1>
             <hr />
 
+
             <div className="row">
                 <div className="col-5">
                     <h4>Search Form</h4>
                     <form onSubmit={handleSearch}>
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="Find your hero" 
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Find your hero"
                             name='query'
-                            value={ query }
+                            value={query}
                             autoComplete='off'
                             onChange={handleInputChange}
-                            />
-                        <button 
+                        />
+                        <button
                             type='submit'
                             className="btn m-1 btn-block btn-outline-primary"
-                            >
+                        >
                             Search
                         </button>
                     </form>
@@ -62,13 +65,29 @@ export const SearchScreen = ({history}) => {
                     {/* Colocare los resultados de un arreglo aque aun no esta diposnible */}
                     <h4>Results</h4>
                     {
+                        (q === '') 
+                            &&
+                                <div className="alert alert-info">
+                                    Search a hero
+                                </div>
+
+                    }
+                    {
+                        (q !== '' && heroesFiltered.length === 0) 
+                            &&
+                                <div className="alert alert-danger">
+                                    There is no a hero with {q}
+                                </div>
+
+                    }
+                    {
                         // Ho puedes usar  { }  en las funciones de array ex map( obj => (...))
                         heroesFiltered.map(hero => (
-                            
+
                             <HeroCard className='col-2 mt-5'
                                 key={hero.id}
-                                {...hero} 
-                                />
+                                {...hero}
+                            />
                         ))
                     }
                 </div>
